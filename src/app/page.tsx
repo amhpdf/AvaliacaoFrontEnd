8d9329d79cb91fde5a9dcdd3b7e5eb7b6a9f278a
Rafael Soares de Almeida <93219825+rafadealmeida@users.noreply.github.com>
2024-02-23 21:02:00 -0300

    [main] - Base do projeto

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="h-screen flex items-center justify-center">
+      <div>Hello World</div>
+    </main>
+  );
+}
